44a954d1e3de1f4357663e5c118b312df63b8e26
AhmedAfzal2089 <ameafzal@gmail.com>
2025-09-22 16:56:09 +0500

    fix redirect to blog page

--- components/Home/RecentBlogs.tsx
+++ components/Home/RecentBlogs.tsx
@@ -6,22 +6,17 @@ export default function RecentBlogs() {
   return (
     <View className="bg-zinc-950 mx-4 p-6 my-6 py-14 items-center rounded-xl">
       {/* Title */}
-      <Text className="text-2xl font-bold text-center text-white mb-2">
-        Recent Blogs
-      </Text>
+      <Text className="text-2xl font-bold text-center text-white mb-2">Recent Blogs</Text>
 
       {/* Subtitle */}
       <Text className="text-gray-300 text-center mb-6">
-        Discover the latest insights, tips, and expert advice on skincare and
-        aesthetic treatments.
+        Discover the latest insights, tips, and expert advice on skincare and aesthetic treatments.
       </Text>
 
       {/* "VIEW ALL BLOGS" Button */}
-      <Link href="/pages/blogs/blogs" asChild>
+      <Link href="/blogs" asChild>
         <TouchableOpacity className="px-6 py-4 bg-white rounded-md shadow-lg">
-          <Text className="text-zinc-900 font-semibold text-base">
-            VIEW ALL BLOGS
-          </Text>
+          <Text className="text-zinc-900 font-semibold text-base">VIEW ALL BLOGS</Text>
         </TouchableOpacity>
       </Link>
     </View>
